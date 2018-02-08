b826bb9dc149e0c5e6bfb6ad81355b673ad86278
Phasin <phasin.s@ku.th>
2018-02-08 17:59:05 +0700

    add icons

--- src/modules/shares/TabMenu.js
+++ src/modules/shares/TabMenu.js
@@ -1,15 +1,22 @@
+import { Dimensions, StyleSheet, Text, View } from 'react-native'
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
 
 import GlobalPage from '../global/GlobalPage'
 import HomePage from '../home/HomePage'
+import Icon from 'react-native-vector-icons/FontAwesome'
 import NotificationPage from '../notification/NotificationPage'
 import TabNavigator from 'react-native-tab-navigator'
 import UserPage from '../user/UserPage'
 
+const deviceWidth = Dimensions.get('window').width
+const basePx = 375
 export default class TabMenu extends Component {
   state= {
     selectedTab: 'home'
+  }
+
+  px2dp(px) {
+    return px * deviceWidth / basePx
   }
 
   render () {
@@ -20,6 +27,8 @@ export default class TabMenu extends Component {
           title="Home"
           selectedTitleStyle={{color: "#3496f0"}}
           onPress={() => this.setState({selectedTab: 'home'})}
+          renderIcon={() => <Icon name="home" size={this.px2dp(22)} color="#666"/>}
+          renderSelectedIcon={() => <Icon name="home" size={this.px2dp(22)} color="#3496f0"/>}
         >
           <HomePage/>
         </TabNavigator.Item>
@@ -28,6 +37,8 @@ export default class TabMenu extends Component {
           title="Global"
           selectedTitleStyle={{color: "#3496f0"}}
           onPress={() => this.setState({selectedTab: 'global'})}
+          renderIcon={() => <Icon name="globe" size={this.px2dp(22)} color="#666"/>}
+          renderSelectedIcon={() => <Icon name="globe" size={this.px2dp(22)} color="#3496f0"/>}
         >
           <GlobalPage/>
         </TabNavigator.Item>
@@ -36,14 +47,18 @@ export default class TabMenu extends Component {
           title="Notification"
           selectedTitleStyle={{color: "#3496f0"}}
           onPress={() => this.setState({selectedTab: 'notification'})}
+          renderIcon={() => <Icon name="bell" size={this.px2dp(22)} color="#666"/>}
+          renderSelectedIcon={() => <Icon name="bell" size={this.px2dp(22)} color="#3496f0"/>}
         >
           <NotificationPage/>
         </TabNavigator.Item>
         <TabNavigator.Item
           selected={this.state.selectedTab === 'user'}
-          title="UserProfile"
+          title="Profile"
           selectedTitleStyle={{color: "#3496f0"}}
           onPress={() => this.setState({selectedTab: 'user'})}
+          renderIcon={() => <Icon name="user" size={this.px2dp(22)} color="#666"/>}
+          renderSelectedIcon={() => <Icon name="user" size={this.px2dp(22)} color="#3496f0"/>}
         >
           <UserPage/>
         </TabNavigator.Item>
