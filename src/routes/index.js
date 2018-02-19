5f1bdf67b33bc689387cab9b8704396fc59332a7
gunhappy <phasin.s@ku.th>
2018-02-19 14:40:35 +0700

    change laucher.js to use router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
-import { Router, Scene } from 'react-native-router-flux';
-import HomePage from '../modules/home/HomePage'
-import GlobalPage from '../modules/global/GlobalPage'
+import { Router, Scene } from 'react-native-router-flux'
+import HomePage from 'src/modules/home/HomePage'
+import GlobalPage from 'src/modules/global/GlobalPage'
+import TabMenu from 'src/modules/shares/TabMenu'
 
 const App = () => {
   return(
     <Router>
       <Scene key="root">
-        <Scene key="homePage" component={HomePage} hideNavBar={1} panHandlers={null} hideTabBar={1} direction='vertical' initial/>
+        <Scene key="homePage" component={HomePage} hideNavBar={1} panHandlers={null} hideTabBar={1} direction='vertical'/>
         <Scene key="globalPage" component={GlobalPage} hideNavBar={1} panHandlers={null} hideTabBar={1} direction='vertical'/>
+        <Scene key="tabMenu" component={TabMenu} hideNavBar={1} panHandlers={null} hideTabBar={1} direction='vertical' initial/>
       </Scene>
     </Router>
   );
