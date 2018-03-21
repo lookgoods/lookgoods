b21294354b8afa5edfafbdfde345725cdf10d425
paiizz <wanchanapon.t@ku.th>
2018-03-22 01:33:26 +0700

    mock ui notification

--- src/constant/images.js
+++ src/constant/images.js
@@ -3,5 +3,6 @@ export default images = {
     product2: require('src/assets/images/products/product2.png'),
     product3: require('src/assets/images/products/product3.jpg'),
     product4: require('src/assets/images/products/product4.jpg'),
+    product5: require('src/assets/images/products/product5.jpg'),
     profile: require('src/assets/images/profile.jpg')
 }
