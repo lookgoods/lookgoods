2f6065e74a519f57dd3867d554e6d50234f9137c
paiizz <wanchanapon.t@ku.th>
2018-03-22 01:33:26 +0700

    mock ui notification

--- src/modules/notification/NotificationPage.js
+++ src/modules/notification/NotificationPage.js
@@ -2,34 +2,76 @@ import {
 	Platform,
 	StyleSheet,
 	Text,
-	View
+	View,
+	PixelRatio,
+	TouchableOpacity,
+	Image
 } from 'react-native'
 import React, { Component } from 'react'
-
+import CoverImage from 'src/modules/shares/CoverImage'
 import NavBar from 'src/modules/shares/NavBar'
+import images from 'src/constants/images'
 
 export default class NotificationPage extends Component {
-	constructor (props) {
+	constructor(props) {
 		super(props)
-    
 	}
-  
+
 	render() {
 		return (
 			<View style={styles.container}>
 				<View style={styles.body}>
-					<Text>Notification</Text>
+					<TouchableOpacity style={styles.list}>
+						<CoverImage size={80} url={images.profile} />
+						<View style={{ marginLeft: 13, flex: 1 }}>
+							<View style={{}}>
+								{/* <Text style={styles.textName}>Phasin Sarunpornkul </Text> */}
+								<Text style={{ fontSize: 15, marginBottom: 4 }}>
+									Phasin Sarunpornkul commented on a review HyperX Cloud headset
+									สุดยอดหูฟังเเนวหน้าของวงการ
+								</Text>
+							</View>
+							<Text style={[styles.font15, { marginBottom: 4 }]}>
+								4 minutes ago
+							</Text>
+						</View>
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.list}>
+						<View style={styles.containerImage}>
+							<Image
+								style={{
+									width: 85,
+									height: 85,
+									resizeMode: 'cover',
+									borderWidth: 1,
+									borderRadius: 3,
+									borderColor: '#f1f1f1'
+								}}
+								source={images.product5}
+								resizeMode="cover"
+							/>
+						</View>
+						<View style={{ marginLeft: 13, flex: 1 }}>
+							<Text style={[styles.font15, { marginBottom: 4 }]}>
+								Phasin Sarunpornkul added a review HyperX Cloud headset
+								สุดยอดหูฟังเเนวหน้าของวงการ
+							</Text>
+							<Text style={[styles.font15, { marginBottom: 4 }]}>
+								4 minutes ago
+							</Text>
+						</View>
+					</TouchableOpacity>
 				</View>
 				<View style={styles.header}>
 					<View style={styles.platformHeader}>
-						<NavBar titleName="NotificationPage"/>
+						<NavBar titleName="NotificationPage" />
 					</View>
 				</View>
 			</View>
 		)
 	}
 }
-  
+
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
@@ -50,5 +92,33 @@ const styles = StyleSheet.create({
 		backgroundColor: 'transparent',
 		overflow: 'hidden',
 		zIndex: 1
+	},
+	list: {
+		padding: 15,
+		flexDirection: 'row',
+		alignItems: 'center',
+		borderColor: '#f1f1f1',
+		borderWidth: 1 / PixelRatio.get(),
+		height: 100
+	},
+	// coverIcon: {
+	// 	height: 30,
+	// 	width: 30,
+	// 	borderWidth: 1.3,
+	// 	borderRadius: 15,
+	// 	justifyContent: 'center',
+	// 	alignItems: 'center',
+	// 	backgroundColor: '#FFF',
+	// 	borderColor: '#c8c8c8',
+	// 	marginRight: 5
+	// },
+	// textName: {
+	// 	fontSize: 15,
+	// 	fontWeight: 'bold',
+	// 	marginBottom: 4
+	// },
+	containerImage: {
+		alignItems: 'center',
+		width: 85
 	}
 })
