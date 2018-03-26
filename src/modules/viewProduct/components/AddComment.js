b4134acade794dd2f14fc7801074419dbd21617e
paiizz <wanchanapon.t@ku.th>
2018-03-27 00:12:53 +0700

    fix send button

--- src/modules/viewProduct/components/AddComment.js
+++ src/modules/viewProduct/components/AddComment.js
@@ -13,7 +13,7 @@ import IconFontAwesome from 'react-native-vector-icons/FontAwesome'
 import { colors } from 'src/constants/mixins'
 
 export default class AddComment extends Component {
-	constructor (props) {
+	constructor(props) {
 		super(props)
 		this.state = {
 			message: '',
@@ -24,50 +24,76 @@ export default class AddComment extends Component {
 
 	changeRating(index) {
 		var stars = []
-		for (let i=0; i<5; i++) {
-			if (i<index+1) stars.push('star')
-			else stars.push('star-o') 
+		for (let i = 0; i < 5; i++) {
+			if (i < index + 1) stars.push('star')
+			else stars.push('star-o')
 		}
 
 		this.setState({
-			rating: index+1,
+			rating: index + 1,
 			stars: stars
 		})
 	}
 
 	render() {
-		if (!this.props.user) return <View/>
+		if (!this.props.user) return <View />
 		else {
 			const { picture_url } = this.props.user
 			return (
 				<View style={{ marginTop: 10, marginBottom: 50 }}>
 					<View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-						<CoverImage uri={picture_url} size={80}/>
+						<CoverImage uri={picture_url} size={80} />
 					</View>
-					<View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: 10 }}>
-						<Text style={{ position: 'absolute', left: 20, fontSize: 12 }}>Tap to rate</Text>
+					<View
+						style={{
+							flexDirection: 'row',
+							justifyContent: 'center',
+							alignItems: 'center',
+							marginTop: 10
+						}}
+					>
+						<Text style={{ position: 'absolute', left: 20, fontSize: 12 }}>
+							Tap to rate
+						</Text>
 						<View style={{ flexDirection: 'row' }}>
 							{this.state.stars.map((item, index) => (
-								<TouchableOpacity key={index} onPress={() => this.changeRating(index)}>
-									<IconFontAwesome style={styles.star} name={this.state.stars[index]} size={40} color={colors.yellow}/>
+								<TouchableOpacity
+									key={index}
+									onPress={() => this.changeRating(index)}
+								>
+									<IconFontAwesome
+										style={styles.star}
+										name={this.state.stars[index]}
+										size={40}
+										color={colors.yellow}
+									/>
 								</TouchableOpacity>
 							))}
 						</View>
 					</View>
-					{ this.state.rating !== 0 &&
+					{this.state.rating !== 0 && (
 						<View style={{ flexDirection: 'row', marginTop: 10 }}>
-							<View style={{ marginLeft: 10, marginRight: 20, backgroundColor: colors.lightGray }}>
+							<View
+								style={{
+									marginLeft: 10,
+									marginRight: 20,
+									backgroundColor: colors.lightGray
+								}}
+							>
 								<TextInput
-									placeholder='Add a comment'
+									placeholder="Add a comment"
 									value={this.state.message}
-									onChangeText={(message) => this.setState({ message })}
+									onChangeText={message => this.setState({ message })}
 									style={styles.textInput}
-									underlineColorAndroid='transparent'
+									underlineColorAndroid="transparent"
 								/>
 							</View>
-							<Button title='Send' color={colors.orange}/>
+							<TouchableOpacity style={styles.buttonSend}>
+								<Text style={styles.fontSend}>Send</Text>
+							</TouchableOpacity>
+							{/* <Button title="Send" color={colors.orange} /> */}
 						</View>
-					}
+					)}
 				</View>
 			)
 		}
@@ -83,5 +109,16 @@ const styles = StyleSheet.create({
 		width: 300,
 		padding: 10,
 		borderRadius: 5
+	},
+	fontSend: {
+		fontSize: 18,
+		fontWeight: 'bold',
+		color: colors.orange
+	},
+	buttonSend: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		flexDirection: 'row',
+		borderRadius: 3
 	}
 })
