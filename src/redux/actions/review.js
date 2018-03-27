5bdfbfc21846ddf6aa0fccf84de9664b537a7713
gunhappy <phasin.s@ku.th>
2018-03-27 16:10:46 +0700

    add review action

--- src/redux/actions/review.js
+++ src/redux/actions/review.js
@@ -1,10 +1,20 @@
 import constants from 'src/redux/constants'
+import axios from 'react-native-axios'
+import to from 'await-to-js'
+
+const AppURL = constants.AppURL
 
 const ReviewActions = {
 	setCurrentReview: (review) => ({
 		type: constants.SET_CURRENT_REVIEW,
 		payload: review
-	})
+	}),
+	addReview: (review) => async dispatch => {
+
+	}
+}
+
+const actions = {
 }
 
 export default ReviewActions
