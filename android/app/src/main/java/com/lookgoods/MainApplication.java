f7cb876870e0c12ff768e5cc56bc833e0b1e33aa
gunhappy <phasin.s@ku.th>
2018-04-10 14:19:51 +0700

    fix android run error

--- android/app/src/main/java/com/lookgoods/MainApplication.java
+++ android/app/src/main/java/com/lookgoods/MainApplication.java
@@ -47,10 +47,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
               new MainReactPackage(),
-            new VectorIconsPackage(),
-            new ImagePickerPackage(),
-            new PickerPackage(),
-            new FBSDKPackage(),
             new PickerPackage(),
             new VectorIconsPackage(),
             new ImagePickerPackage(),
